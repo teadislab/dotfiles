005f8dcba3f349adc1b163d0198f9c1351aa4a8b
teadi <Aditya.patil0404@gmail.com>
2022-07-01 23:12:31 +0530

    Updated

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh
 
 echo 'Installing Lazygit'
 add-apt-repository ppa:lazygit-team/daily
 apt-get update
 apt-get install lazygit
-
-sh -c "$(curl -fsSL https://starship.rs/install.sh)"
