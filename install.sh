29d39d43cc49801156c72694e8ae30d6fd5bff87
teadi <Aditya.patil0404@gmail.com>
2022-07-06 22:11:52 +0530

    Updated

--- install.sh
+++ install.sh
@@ -17,4 +17,5 @@ sudo curl -L https://github.com/lemnos/tt/releases/download/v0.4.2/tt-linux -o /
 
 sudo curl -o /usr/share/man/man1/tt.1.gz -L https://github.com/lemnos/tt/releases/download/v0.4.2/tt.1.gz
 
-echo 'Dev'
+echo 'Installing Atuin'
+bash <(curl https://raw.githubusercontent.com/ellie/atuin/main/install.sh)
