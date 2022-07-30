4781aed9af926d89415846559b896cd28bdd03eb
Aditya <Aditya.patil0404@gmail.com>
2022-07-30 21:56:22 +0530

    Updated

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 
+echo "Installing Homebrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
@@ -25,3 +28,4 @@ echo 'creating Dev dir'
 mkdir dev
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
